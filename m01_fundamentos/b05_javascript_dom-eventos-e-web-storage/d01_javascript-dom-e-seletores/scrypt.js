a91f5ee8319985398d9389d39ee6a60b9d5ce2f5
Elienay-Alves <elienay8486@gmail.com>
2021-12-03 13:56:37 -0300

    Parte de getElementById feita

---
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d01_javascript-dom-e-seletores/scrypt.js
@@ -0,0 +1,8 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "green";
+
+document.getElementById("page-title").innerText = "The Witcher 3: Wild Hunt";
+
+document.getElementById("second-paragraph").innerText = "O BRUXÃO vai te pegar!!!";
+
+document.getElementById("subtitle").innerText = 'CUIDADO !!!';
