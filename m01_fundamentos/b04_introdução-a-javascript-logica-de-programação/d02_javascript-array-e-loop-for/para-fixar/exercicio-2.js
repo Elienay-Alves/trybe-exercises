b0943aeff8b2c8335254b826b43a24da6acb8aca
Elienay-Alves <elienay8486@gmail.com>
2021-12-05 02:51:12 -0300

    exercicios para fixar do loop feito

---
+++ m01_fundamentos/b04_introdução-a-javascript-logica-de-programação/d02_javascript-array-e-loop-for/para-fixar/exercicio-2.js
@@ -0,0 +1,12 @@
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for ( let index = 0; index < index.length; index += 1) {
+//   console.log(groceryList[index]);
+// }
+// console.log (groceryList);
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let word of names) {
+  console.log(word)
+}
