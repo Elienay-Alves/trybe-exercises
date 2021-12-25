d3136d5813e3ce5f2cf0b93417ffd4ff56537b3b
Elienay-Alves <elienay8486@gmail.com>
2021-12-25 11:52:12 -0300

    adicionei a bornInfo e consolei

--- m01_fundamentos/b04_introdução-a-javascript-logica-de-programação/d04_objetos-e-funções/exercicios-de-videos/objetos.js
+++ m01_fundamentos/b04_introdução-a-javascript-logica-de-programação/d04_objetos-e-funções/exercicios-de-videos/objetos.js
@@ -4,6 +4,10 @@ let singer = {
   nickname: 'Bituca',
   age: 77,
   bestAlbuns: ['Travessia', 'Clube da Esquina', 'Minas'],
+  bornInfo: {
+    city: 'Rio de Janeiro',
+    state: 'Rio de Janeiro',
+  }
 };
 
 console.log('O cantor ' + singer.name + ' ' + singer.lastName + ' possui ' + singer.age + ' anos.');
@@ -12,4 +16,4 @@ singer ['fullName'] = singer.name + ' ' + singer.lastName;
 
 console.table(singer);
 
-arroz e feijão
+console.log ('O cantor ' + singer.fullName + ' nasceu no estado do ' + singer.bornInfo.state);
