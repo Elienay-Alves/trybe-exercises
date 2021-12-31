d9982552844796326080d416bc30720925d9c92a
Elienay-Alves <elienay8486@gmail.com>
2021-12-31 13:43:26 -0300

    Script.js modificado e alterado para source correta

--- m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/para-fixar/script1.js
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/para-fixar/script1.js
@@ -1,10 +1,10 @@
-let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+// // let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
-let pai = elementoOndeVoceEsta.parentElement;
-pai.style.color = 'red';
+// // let pai = elementoOndeVoceEsta.parentElement;
+// pai.style.color = 'red';
 
-let primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
-primeiroFilhoDoFilho.innerText = 'Primeiro filho do filho';
+// let primeiroFilhoDoFilho = elementoOndeVoceEsta.firstElementChild;
+// primeiroFilhoDoFilho.innerText = 'Primeiro filho do filho';
 
 let primeiroFilho = pai.firstElementChild;
 
@@ -14,5 +14,36 @@ let atencao = elementoOndeVoceEsta.nextSibling;
 
 let thirdChild = elementoOndeVoceEsta.nextElementSibling;
 
-let terceiroFilho = pai.lastElementChild.previousElementSibling;
+// let terceiroFilho = pai.lastElementChild.previousElementSibling;
 
+// Parte II
+// 1. Crie um irmão para `elementoOndeVoceEsta`.
+
+let pai = document.getElementById('pai');
+let irmaoElementoOndeVoceEsta = document.createElement('section');
+
+irmaoElementoOndeVoceEsta.id = 'irmaoElementoOndeVoceEsta';
+pai.appendChild(irmaoElementoOndeVoceEsta);
+
+// 2. Crie um filho para 'elementoOndeVoceEsta'.
+
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+
+let filhoElementoOndeVoceEsta = document.createElement('section');
+
+filhoElementoOndeVoceEsta.id = 'filhoElementoOndeVoceEsta';
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+
+// 3. Crie um filho para 'primeiroFilhoDoFilho'.
+
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+let filhoPrimeiroFilhoDoFilho = document.createElement('section');
+
+filhoPrimeiroFilhoDoFilho.id = 'filhoPrimeiroFilhoDoFilho';
+primeiroFilhoDoFilho.appendChild(filhoPrimeiroFilhoDoFilho);
+
+// 4. A partir desse filho criado, acesse 'terceiroFilho'.
+
+let terceiroFilho = filhoPrimeiroFilhoDoFilho.parentElement.parentElement.nextElementSibling;
+
+console.log(terceiroFilho)
