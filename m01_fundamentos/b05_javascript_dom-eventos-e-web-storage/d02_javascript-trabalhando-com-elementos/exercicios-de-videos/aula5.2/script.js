934d35a78d8637652f5884d8526a2c6d5c368a62
Elienay-Alves <elienay8486@gmail.com>
2022-01-01 22:25:05 -0300

    criei uma div na linha 6

--- m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
@@ -1,3 +1,6 @@
 // Onde eu quero adicionar esse elemento?
 let container = document.getElementById('first');
 console.log(container);
+
+// Como criar um elemento HTML?
+let newDiv = document.createElement('div')
