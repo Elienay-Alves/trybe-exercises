eb67e53fbee5fad793fc565bed9f8b2669f3cbfa
Elienay-Alves <elienay8486@gmail.com>
2022-01-01 22:48:03 -0300

    fizemos mais duas divs dinamicas

--- m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
@@ -4,9 +4,16 @@ console.log(container);
 
 // Como criar um elemento HTML?
 let newDiv = document.createElement('div')
+let newDiv2 = document.createElement('div')
+let newDiv3 = document.createElement('div')
 console.log(newDiv);
 
 // Como adicionar classes a este elemento?
 newDiv.className = 'circle small green';
+newDiv2.className = 'square big red';
+newDiv3.className = 'circle medium yellow';
 
+// Como pôr o elemento no arquivo HTML?
 container.appendChild(newDiv);
+container.appendChild(newDiv2);
+container.appendChild(newDiv3);
