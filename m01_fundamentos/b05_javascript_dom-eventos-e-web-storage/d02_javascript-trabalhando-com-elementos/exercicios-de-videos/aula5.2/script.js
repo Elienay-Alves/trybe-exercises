6b3cfacd34d7d24d79845ea9e65c80bdac0a8f05
Elienay-Alves <elienay8486@gmail.com>
2022-01-01 23:02:55 -0300

    fizemos como uma funçao esse codigo

--- m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
@@ -2,18 +2,39 @@
 let container = document.getElementById('first');
 console.log(container);
 
-// Como criar um elemento HTML?
-let newDiv = document.createElement('div')
-let newDiv2 = document.createElement('div')
-let newDiv3 = document.createElement('div')
-console.log(newDiv);
+// function createDivElement (classes) {
+//   let newDiv = document.createElement('div');
+//   newDiv.className = classes;
+//   container.appendChild(newDiv);
+// }
 
-// Como adicionar classes a este elemento?
-newDiv.className = 'circle small green';
-newDiv2.className = 'square big red';
-newDiv3.className = 'circle medium yellow';
+// createDivElement('circle small red');
 
-// Como pôr o elemento no arquivo HTML?
-container.appendChild(newDiv);
-container.appendChild(newDiv2);
-container.appendChild(newDiv3);
+// Eu fiz da forma acima e funcionou, porém o professor fez de outra forma. Como esta abaixo:
+
+function createDivElement (classes) {
+  let newDiv = document.createElement('div');
+  newDiv.className = classes;
+  return newDiv
+}
+
+container.appendChild(createDivElement('circle small red'));
+container.appendChild(createDivElement('square small green'));
+container.appendChild(createDivElement('circle small yellow'));
+
+// Jeito velho de fazer:
+
+// let newDiv = document.createElement('div')
+// let newDiv2 = document.createElement('div')
+// let newDiv3 = document.createElement('div')
+// console.log(newDiv);
+
+// // Como adicionar classes a este elemento?
+// newDiv.className = 'circle small green';
+// newDiv2.className = 'square big red';
+// newDiv3.className = 'circle medium yellow';
+
+
+// container.appendChild(newDiv);
+// container.appendChild(newDiv2);
+// container.appendChild(newDiv3);
