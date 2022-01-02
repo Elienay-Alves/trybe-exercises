21893811d50181d82fd3ce064b0f3594850b555b
Elienay-Alves <elienay8486@gmail.com>
2022-01-01 22:32:52 -0300

    Fizemos o appendChild

--- m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
+++ m01_fundamentos/b05_javascript_dom-eventos-e-web-storage/d02_javascript-trabalhando-com-elementos/exercicios-de-videos/aula5.2/script.js
@@ -4,3 +4,9 @@ console.log(container);
 
 // Como criar um elemento HTML?
 let newDiv = document.createElement('div')
+console.log(newDiv);
+
+// Como adicionar classes a este elemento?
+newDiv.className = 'circle small green';
+
+container.appendChild(newDiv);
