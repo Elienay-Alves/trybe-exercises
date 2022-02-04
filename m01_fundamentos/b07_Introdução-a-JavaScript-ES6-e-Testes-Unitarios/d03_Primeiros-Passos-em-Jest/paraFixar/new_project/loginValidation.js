1556db4318ecda7f90d81fe1469182bbe307b0e3
Elienay-Alves <elienay8486@gmail.com>
2022-02-04 16:24:47 -0300

    Estudando

--- m01_fundamentos/b07_Introdução-a-JavaScript-ES6-e-Testes-Unitarios/d03_Primeiros-Passos-em-Jest/paraFixar/new_project/loginValidation.js
+++ m01_fundamentos/b07_Introdução-a-JavaScript-ES6-e-Testes-Unitarios/d03_Primeiros-Passos-em-Jest/paraFixar/new_project/loginValidation.js
@@ -1,11 +1,7 @@
 // loginValidation.js
-const greetingMessage = (user) => {
-  return `Hello, ${user}! Que bom ter você de volta`;
-};
+const greetingMessage = (user) => `Hello, ${user}! Que bom ter você de volta`;
 
-const loginErrorMessage = (user) => {
-  return `Pessoa usuária '${user}' não encontrada, tente novamente!`;
-};
+const loginErrorMessage = (user) =>`Pessoa usuária '${user}' não encontrada, tente novamente!`;
 
 const user = {
   userName: "Joana",
