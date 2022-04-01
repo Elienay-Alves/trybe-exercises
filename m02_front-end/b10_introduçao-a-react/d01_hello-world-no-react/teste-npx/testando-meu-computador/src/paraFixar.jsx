d7b6d4394c4cf75b1619b7c02ad539158ee65a6a
Elienay-Alves <elienay8486@gmail.com>
2022-04-01 14:50:20 -0300

    Refazendo módulo de Frontend

---
+++ m02_front-end/b10_introduçao-a-react/d01_hello-world-no-react/teste-npx/testando-meu-computador/src/paraFixar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class JSX extends Component {
+  render() {
+    const textJSX = 'Hello, JSX';
+    return (
+      <div>
+        <h1>{ textJSX }</h1>
+      </div>
+    )
+  }
+}
+
+export default JSX;
