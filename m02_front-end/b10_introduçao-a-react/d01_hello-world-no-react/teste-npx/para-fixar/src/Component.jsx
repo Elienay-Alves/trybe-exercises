7b96c2b0c27c7018fb9dea988d769456c0680b8c
Elienay-Alves <elienay8486@gmail.com>
2022-04-01 15:07:13 -0300

    Refazendo módulo de Frontend

---
+++ m02_front-end/b10_introduçao-a-react/d01_hello-world-no-react/teste-npx/para-fixar/src/Component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Description extends Component {
+  render() {
+    return(
+      <div>
+        <h1>Elienay Alves</h1>
+        <p>Esse é meu nome e eu estudo Desenvolvimento Web na Trybe</p>
+      </div>
+    )
+  }
+}
