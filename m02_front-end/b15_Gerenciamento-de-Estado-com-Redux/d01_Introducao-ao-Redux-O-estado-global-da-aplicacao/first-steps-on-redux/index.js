b9e28c893a0fcab1ab2a80183325ecbfaa507a48
Elienay-Alves <elienay8486@gmail.com>
2022-04-05 11:53:51 -0300

    Reescrevendo Estado Inicial

--- m02_front-end/b15_Gerenciamento-de-Estado-com-Redux/d01_Introducao-ao-Redux-O-estado-global-da-aplicacao/first-steps-on-redux/index.js
+++ m02_front-end/b15_Gerenciamento-de-Estado-com-Redux/d01_Introducao-ao-Redux-O-estado-global-da-aplicacao/first-steps-on-redux/index.js
@@ -1,8 +1,14 @@
 // ################### IPORTAÇÃO DO REDUX ############################
 const Redux = require('redux');
 
+// ################### ESCREVENDO ESTADO INICIAL ####################
+const ESTADO_INICIAL = {
+  login: false,
+  email: '',
+}
+
 // ################### MONTANDO O REDUCER ############################
-const reducer = (state = { login: false, email: '' }) => state;
+const reducer = (state = ESTADO_INICIAL) => state;
 
 // ################### CRIAÇÃO DA STORE ##############################
 const store = Redux.createStore(reducer);
