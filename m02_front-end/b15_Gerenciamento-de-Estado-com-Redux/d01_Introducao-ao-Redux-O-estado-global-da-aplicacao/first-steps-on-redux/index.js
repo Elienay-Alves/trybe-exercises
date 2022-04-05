9501b6028dbdf165efa41c48777c36d8cce17949
Elienay-Alves <elienay8486@gmail.com>
2022-04-05 11:49:00 -0300

    Montei o reducer

--- m02_front-end/b15_Gerenciamento-de-Estado-com-Redux/d01_Introducao-ao-Redux-O-estado-global-da-aplicacao/first-steps-on-redux/index.js
+++ m02_front-end/b15_Gerenciamento-de-Estado-com-Redux/d01_Introducao-ao-Redux-O-estado-global-da-aplicacao/first-steps-on-redux/index.js
@@ -1,1 +1,9 @@
+// ################### IPORTAÇÃO DO REDUX ############################
 const Redux = require('redux');
+
+// ################### MONTANDO O REDUCER ############################
+const reducer = (state) => state;
+
+// ################### CRIAÇÃO DA STORE ##############################
+const store = Redux.createStore();
+
