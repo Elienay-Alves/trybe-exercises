b3a79a9eb98c1b2c23da23b581451ee7ea087e93
Elienay-Alves <elienay8486@gmail.com>
2022-06-27 21:19:24 -0300

    adiciona requisito 6 do exercicio

--- m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/exercicio-do-dia/src/velocidade.js
+++ m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/exercicio-do-dia/src/velocidade.js
@@ -1,21 +1,17 @@
-const { questionInt, questionFloat } = require('readline-sync');
-
-const distancia = questionInt('Qual a distancia?');
-const tempo = questionInt('Qual o tempo tempo em segundos?');
+const { questionInt } = require('readline-sync');
 
 const calculaDist = (d, t) => {
-  console.log(`Distancia: ${d}, Tempo:${t}`);
-
   const media = (d / t);
 
   return media
-
-  
 };
 
 const main = () => {
-  const imc = calculaDist(distancia, tempo);
-  console.log(`Média: ${imc.toFixed(2)}`);
+  const distancia = questionInt('Qual a distancia?');
+  const tempo = questionInt('Qual o tempo tempo em segundos?');
+  
+  const velMed = calculaDist(distancia, tempo);
+  console.log(`Média: ${velMed.toFixed(2)}`);
 }
 
 main();
