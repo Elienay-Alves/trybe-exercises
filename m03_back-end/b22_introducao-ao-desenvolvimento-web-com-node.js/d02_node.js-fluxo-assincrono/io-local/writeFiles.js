abb7c404d468c105f0cb9173ed0d6dd6eeebb5d6
Elienay-Alves <elienay8486@gmail.com>
2022-06-29 18:37:38 -0300

    adicionado forma de escrever arquivos com metodos asincronos com async awayt

--- m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/io-local/writeFiles.js
+++ m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/io-local/writeFiles.js
@@ -1,3 +1,4 @@
+/*
 const { writeFile } = require('fs').promises;
 
 writeFile('./my-created-file.txt', 'I don\'t believe you are here again kkk')
@@ -7,3 +8,19 @@ writeFile('./my-created-file.txt', 'I don\'t believe you are here again kkk')
   .catch((err) => {
     console.error(`Error writing the file: ${err.message}`);
   });
+*/
+
+//---------- Agora utilizando Async/Await ---------//
+
+const { writeFile } = require('fs').promises;
+
+const main = async () => {
+  try {
+    await writeFile('./my-created-file.txt', 'I don\'t believe you are here again kkk');
+    console.log('File created with sucess!');
+  } catch (err) {
+    console.error(`Error writing the file: ${err.message}`);
+  }
+}
+
+main();
