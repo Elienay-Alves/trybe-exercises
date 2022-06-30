1ddb740fafb080361351863de3066ae215933083
Elienay-Alves <elienay8486@gmail.com>
2022-06-30 17:56:36 -0300

    adicionado exercicio2

--- m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/exercicios-do-dia/src/index.js
+++ m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d02_node.js-fluxo-assincrono/exercicios-do-dia/src/index.js
@@ -1,20 +1,32 @@
-
-const exercise01 = (a, b, c) => {
+const mathQuestion = (a, b, c) => {
   const promise = new Promise((resolve, reject) => {
-    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') reject(new Error('Informe apenas números'));
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      return reject(new Error('Informe apenas números'));
+    }
 
-    const result = (a + b) * c;
+    const result = (a + b)* c;
 
     if (result < 50) {
-      return reject(new Error('Valor muito baixo'))
+      return reject(new Error('Valor muito baixo'));
     }
 
     resolve(result);
   })
 
-  return promise;
+  return promise
 }
 
-exercise01(10, 20, 's')
+// mathQuestion(21, 11, 5)
+//   .then(console.log)
+//   .catch(err => console.error(err.message));
+
+const randomParameter = () => {
+  const random = Math.floor(Math.random() * 100 + 1);
+  const funct = mathQuestion(random, random, random);
+
+  return funct;
+}
+
+randomParameter()
   .then(console.log)
-  .catch(error => console.log(error.message));
+  .catch(err => console.error(err.message))
