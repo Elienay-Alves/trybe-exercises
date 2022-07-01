37f5bf799f2faa354a51b38451fee8ec37303f6a
Elienay-Alves <elienay8486@gmail.com>
2022-07-01 14:33:18 -0300

    adicionado exemplos da sessao "Aplicando Stubs ao nosso exemplo"

--- m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d03_mocha-chai-e-sinon-testes-de-back-end-com-node.js/examples/io-test/test.js
+++ m03_back-end/b22_introducao-ao-desenvolvimento-web-com-node.js/d03_mocha-chai-e-sinon-testes-de-back-end-com-node.js/examples/io-test/test.js
@@ -1,27 +1,51 @@
+const fs = require('fs');
+const sinon = require('sinon')
 const { expect } = require('chai');
 const readFile = require('./readFile');
 
 const FILE = 'The Quarry';
 
+
 describe('Read file', () => {
   describe('When the file exists', () => {
-    describe('the answer', () => {
-      const answer = readFile('file.txt');
+    before(() => {
+      sinon.stub(fs, 'readFileSync').returns(FILE);
+    });
 
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
+    describe('the answer', () => {
       it('it\'s a string', () => {
+        const answer = readFile('file.txt');
+
         expect(answer).to.be.a('string');
       });
 
       it('it\'s equal the file content', () => {
+        const answer = readFile('file.txt');
+
         expect(answer).to.be.equals(FILE);
       });
     });
   });
 
   describe('When the file don\'t exists', () => {
+    before(() => {
+      sinon
+        .stub(fs, 'readFileSync')
+        .throws(new Error('File not found'));
+    });
+
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
     describe('the answer', () => {
-      const answer = readFile('file_that_do_not_exists.txt');
       it('it\'s equal "null"', () => {
+        const answer = readFile('file_that_do_not_exists.txt');
+        
         expect(answer).to.be.equal(null);
       });
     });
