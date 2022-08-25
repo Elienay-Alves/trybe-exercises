cce45d054fd1ca03d957252e6df128664908dea1
Elienay-Alves <elienay8486@gmail.com>
2022-08-25 11:42:59 -0300

    commit bomba

---
+++ m03_back-end/b26_typescript/d01_introducao-ao-typescript/example1/exercise1.ts
@@ -0,0 +1,35 @@
+enum WeekDays {
+  Sunday = 1,
+  Monday = 2,
+  Tuesday = 3,
+  Wednesday = 4,
+  Thursday = 5,
+  Friday = 6,
+  Saturday = 7,
+}
+
+enum RainbowColors {
+  firstColor = 'red',
+  secondColor = 'orange',
+  thirdColor = 'yellow',
+  fourthColor = 'green',
+  fifthColor = 'blue',
+  sixthColor = 'indigo',
+  seventhColor = 'violet',
+}
+
+enum Printer {
+  salvar = 1,
+  imprimir = 2,
+  abrir = 3,
+  visualizar = 4,
+  fechar = 5,
+}
+
+enum CardinalPoints {
+  norte = 'N',
+  leste = 'L',
+  sul = 'S',
+  oeste = 'O',
+}
+
