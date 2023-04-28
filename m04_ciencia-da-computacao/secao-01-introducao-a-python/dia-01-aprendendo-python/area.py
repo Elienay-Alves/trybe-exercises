c7bd40e124a7884b13e99db7302f79d4dcafb687
Elienay Alves <elienay8486@gmail.com>
2023-04-28 13:48:05 -0300

    Escrevendo os primeiros arquivos

---
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/area.py
@@ -0,0 +1,16 @@
+PI = 3.14
+
+
+def square(side):
+    '''Calcula a área do quadrado'''
+    return side * side
+
+
+def rectangle(length, width):
+    '''Calcula a área do retângulo'''
+    return length * width
+
+
+def circle(radius):
+    '''Calcula a área do circulo'''
+    return PI * radius * radius
