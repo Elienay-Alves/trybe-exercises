a04f32cd4602e30c416ac3a8a3ff1e6d7d722fec
Elienay Alves <elienay8486@gmail.com>
2023-04-29 13:01:38 -0300

    adiciona teste da lib black

---
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/teste_black.py
@@ -0,0 +1,14 @@
+"""
+Este é um módulo de teste da lib black.
+
+Autor: Elienay Alves
+Data: 29/04/2023
+"""
+FELPS = "pessoa instrutora"
+
+
+DICAS_PYTHONICAS_SAO_LEGAIS = True
+
+
+if DICAS_PYTHONICAS_SAO_LEGAIS:
+    print("Mais dicas por favor!!!s")
