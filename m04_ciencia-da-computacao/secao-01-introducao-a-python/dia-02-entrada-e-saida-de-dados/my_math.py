08440cc5f781960b12e2a3ba7ff16ea850b94f9d
Elienay Alves <elienay8486@gmail.com>
2023-05-04 15:12:35 -0300

    Exemplos de importacao e criacao de ambiente

---
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados/my_math.py
@@ -0,0 +1,17 @@
+import http
+
+from http import client
+
+from http.client import HTTP_PORT
+
+
+def sum(a, b):
+    return a + b
+
+
+def pot(a, b):
+    return a**b
+
+
+print(sum(1, 2))
+print(pot(2, 3))
