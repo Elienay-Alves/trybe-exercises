0ea367fe1668883ed65b505939943410386c4bee
Elienay Alves <elienay8486@gmail.com>
2023-05-05 16:24:59 -0300

    adiciona lidando_com_excecoes

---
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados/lidando_com_excecoes.py
@@ -0,0 +1,12 @@
+ALUNOS_RECUPERACAO = []
+
+with open("notas.txt", encoding="utf-8") as file:
+    for line in file:
+        nota_do_aluno = line
+        nota_do_aluno = nota_do_aluno.split(" ")
+        if int(nota_do_aluno[1]) < 6:
+            ALUNOS_RECUPERACAO.append(nota_do_aluno[0] + "\n")
+
+with open("reprovados.txt", mode="w", encoding="utf-8") as final_file:
+    final_file.writelines(ALUNOS_RECUPERACAO)
+    final_file.close()
