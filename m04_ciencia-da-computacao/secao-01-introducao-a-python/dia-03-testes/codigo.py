b49a5d5d55bdddb5e65da4ceb076ba364e229ced
Elienay Alves <elienay8486@gmail.com>
2023-05-10 08:47:10 -0300

    testando falhas

--- m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/codigo.py
+++
@@ -1,3 +0,0 @@
-def is_odd(number):
-    '''Retorna True se um número é impar, senão False'''
-    return number % 2 != 0
--- m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/test_codigo.py
+++
@@ -1,9 +0,0 @@
-from codigo import is_odd
-
-
-def test_is_odd_when_number_is_odd_returns_true():
-    assert is_odd(3) is True
-
-
-def test_is_odd_when_number_even_return_false():
-    assert is_odd(2) is False
