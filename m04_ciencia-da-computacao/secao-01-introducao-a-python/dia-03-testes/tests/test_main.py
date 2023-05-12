bfd1bf7e430e7da6375e64ed6b70f3194cfa6b58
Elienay Alves <elienay8486@gmail.com>
2023-05-12 12:11:50 -0300

    feat(exercicio-01): created def and tested it
    created the function and tested it's functionaity

--- m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/tests/test_main.py
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/tests/test_main.py
@@ -1,4 +1,26 @@
 from src.main import list_returner
 
-def test_list():
-    pass
+def test_list_with_number_by_three():
+    assert list_returner(3) == [1, 2, "Fizz"]
+    
+def test_list_with_number_by_five():
+    assert list_returner(5) == [1, 2, "Fizz", 4, "Buzz"]
+
+def test_list_with_number_by_three_and_five():
+    assert list_returner(15) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+
+def test_list_with_no_parameter():
+    try:
+        list_returner()
+    except TypeError:
+        pass
+    else:
+        raise AssertionError
+    
+def test_list_returner_if_no_int_type():
+    try:
+        list_returner("string")
+    except TypeError:
+        pass
+    else:
+        raise AssertionError
