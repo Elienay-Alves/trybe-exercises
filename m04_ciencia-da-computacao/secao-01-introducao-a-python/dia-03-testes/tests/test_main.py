d6d7c20497a3985160b596e29d5d3a3bfaff8448
Elienay Alves <elienay8486@gmail.com>
2023-05-12 14:48:22 -0300

    style(test_main.py): fix blank line after import on line 3

--- m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/tests/test_main.py
+++ m04_ciencia-da-computacao/secao-01-introducao-a-python/dia-03-testes/tests/test_main.py
@@ -1,13 +1,33 @@
 from src.main import list_returner
 
+
 def test_list_with_number_by_three():
     assert list_returner(3) == [1, 2, "Fizz"]
-    
+
+
 def test_list_with_number_by_five():
     assert list_returner(5) == [1, 2, "Fizz", 4, "Buzz"]
 
+
 def test_list_with_number_by_three_and_five():
-    assert list_returner(15) == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+    assert list_returner(15) == [
+        1,
+        2,
+        "Fizz",
+        4,
+        "Buzz",
+        "Fizz",
+        7,
+        8,
+        "Fizz",
+        "Buzz",
+        11,
+        "Fizz",
+        13,
+        14,
+        "FizzBuzz",
+    ]
+
 
 def test_list_with_no_parameter():
     try:
@@ -16,7 +36,8 @@ def test_list_with_no_parameter():
         pass
     else:
         raise AssertionError
-    
+
+
 def test_list_returner_if_no_int_type():
     try:
         list_returner("string")
