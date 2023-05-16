b4b9ed3a4ce20840843fb21c8d9bc4d1d4479fec
Elienay Alves <elienay8486@gmail.com>
2023-05-15 23:51:52 -0300

    feat(fixation-2): finished fixation 2

--- m04_ciencia-da-computacao/secao-02-padroes-de-projeto/dia-01-poo-em-python/fixation_exercise.py
+++ m04_ciencia-da-computacao/secao-02-padroes-de-projeto/dia-01-poo-em-python/fixation_exercise.py
@@ -1,15 +1,40 @@
-class Fan:
+class Home_appliances:
     def __init__(
         self, color: str, power: int, voltage: int, price: int
     ) -> None:
         self.price = price
         self.__color = color
-        self.__power = power
-        self.__voltage = voltage
+        self._power = power
+        self._voltage = voltage
         self.__on = False
+        self.__speed = 0
+        self.__max_speed = 3
+        self.__current_motor_current = 0
 
-    def __str__(self) -> str:
-        return f"a fan. its color is {self.__color}."
+    def turn_on(self, speed: int) -> None:
+        if speed > self.__max_speed or speed < 0:
+            raise ValueError(f"Speed must be between 0 and {self.__max_speed}")
+
+        self.__speed = speed
+        self.__current_motor_current = (
+            (self._power / self._voltage) / self.__max_speed
+        ) * speed
+        self.__on = True
+
+    def turn_off(self) -> None:
+        self.__on = False
+        self.__speed = 0
+
+    def its_on(self) -> bool:
+        return self.__on
+
+    @property
+    def color(self) -> str:
+        return self.__color.upper()
+
+    @color.setter
+    def color(self, new_color: str) -> None:
+        self.__color = new_color
 
 
 class Person:
@@ -18,19 +43,34 @@ class Person:
         self.account_balance = account_balance
         self.fan = None
 
-    def buy_a_fan(self, fan: Fan) -> None:
-        if fan.price <= self.account_balance:
-            self.account_balance -= fan.price
-            self.fan = fan
 
-    def __str__(self) -> str:
-        if self.fan:
-            return f"{self.name} has {self.fan}"
-        return f"{self.name} don't hava a fan."
+class Fan(Home_appliances):
+    pass
 
 
-white_fan = Fan("white", power=250, voltage=220, price=100)
-person = Person("Elienay", account_balance=2000)
-person.buy_a_fan(white_fan)
+class Dryer(Home_appliances):
+    pass
 
-print(person)
+
+class Mixer(Home_appliances):
+    pass
+
+
+class Washing_machine(Home_appliances):
+    pass
+
+
+if __name__ == "__main__":
+    # person = Person("Elienay", account_balance=2000)
+    # person.buy_a_fan(white_fan)
+    fan = Fan("white", power=250, voltage=220, price=100)
+    dryer = Dryer("red", power=220, voltage=110, price=370)
+    w_machine = Washing_machine("grey", power=250, voltage=220, price=500)
+
+    print(f"I have a fan and payed US${fan.price} for it!")
+    print(
+        f"I had to buy a {dryer.color} dryer and brought one of {dryer._voltage} voltage by mistake!"
+    )
+    print(
+        f"That {w_machine.color} it's so expensive. She payed {w_machine.price} for that, right?"
+    )
